450895cfc0a9074f94b75699dce4a9613ce12428
Ved Chaudhari <vedchaudhari42@gmail.com>
2026-04-22 13:45:01 +0530

    fix: use predictable asset filenames to fix MIME type issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]"
+      }
+    }
+  },
+  publicDir: false,
   server: {
     host: "127.0.0.1",
     proxy: {
